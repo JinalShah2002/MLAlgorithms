d363fe2b34fec65f24041c2ea5abb543df5b1616
Jinal.Shah <jinujets1106@gmail.com>
2020-05-11 23:49:51 -0400

    Creating the Logestic Regression File

---
+++ LogesticRegression/LogesticRegression.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author Jinal Shah
+
+This file will be used to implement
+unregularized Logestic Regression
+
+Reminder, logestic regression is a 
+popular classification algorithm
+
+"""
+# Importing the libraries
+import numpy as np
+
+class LogesticRegression():
+    
+    # Constructor
+    def __init__(self):
+        pass
