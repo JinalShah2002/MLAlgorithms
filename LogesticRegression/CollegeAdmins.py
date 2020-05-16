bbca0843f88281613f2b0e8b3125691a951dd542
Jinal.Shah <jinujets1106@gmail.com>
2020-05-16 00:38:19 -0400

    Completed implementing Gradient Descent for Logestic Regression
    I finished implementing Gradient Descent in Logistic Regression and ran a test case on it.

--- LogesticRegression/CollegeAdmins.py
+++ LogesticRegression/CollegeAdmins.py
@@ -18,12 +18,17 @@ been preprocessed and identified as a classification
 problem. For different data sets, the author will 
 have to follow the Machine Learning Path!
 
+Key Takeaway: Gradient Descent Requires 
+FEATURE SCALING. Make sure to evaluate 
+the features and scale if needed!
+
 """
 # Importing the Libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from LogesticRegression import LogesticRegression
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 # Getting the Data
@@ -42,6 +47,25 @@ y = raw_data['College Admission Decision'].values
 X = X.reshape(100,2)
 y = y.reshape(100,1)
 
-# Building Regressor
+# Plotting the data
+plt.scatter(raw_data['Exam 1 Score'],raw_data['Exam 2 Score'],c=raw_data['College Admission Decision'])
+plt.title('How Scores of 2 exams impact college admission for a certain college')
+plt.xlabel('Exam 1 Score')
+plt.ylabel('Exam 2 Score')
+plt.show()
+
+
+# Building Regressor & Feature Scaling
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 regressor = LogesticRegression()
-regressor.fit(X,y)
+regressor.fit(X_scaled,y)
+regressor.plot()
+
+
+# Predictions
+scaler_2 = StandardScaler()
+exam_1 = np.array(45).reshape(1,1)
+exam_2 = np.array(85).reshape(1,1)
+combined = np.concatenate((exam_1,exam_2),axis=1)
+print(regressor.predict(scaler_2.fit_transform(combined)))
--- LogesticRegression/LogesticRegression.py
+++ LogesticRegression/LogesticRegression.py
@@ -19,7 +19,10 @@ class LogesticRegression():
     
     # Constructor
     def __init__(self):
-      pass
+      self.alpha = 0.01
+      self.iterations = 1500
+      self.cost = []
+      self.iterat = []
     
     # Fit Method
     def fit(self,X,y):
@@ -27,11 +30,10 @@ class LogesticRegression():
         self.X = np.concatenate((ones,X),axis=1)
         self.y = y
         self.theta = np.zeros((self.X.shape[1],1))
-    
+        
     # Sigmoid function
     def sigmoid(self,z):
-        bottom = 1 + np.exp(-z)
-        return 1/bottom
+        return 1.0/(1+np.exp(-z))
     
     # Hypothesis Function
     def getHypothesis(self):
@@ -39,18 +41,71 @@ class LogesticRegression():
         return self.sigmoid(z)
 
     # Getting the Cost Function
-    def getCost(self):
+    def getCost(self):  
         sum1 = np.matmul(-self.y.T,np.log(self.getHypothesis()))
-        sum2 = np.matmul((1-self.y).T,np.log(1-self.getHypothesis()))
+        sum2 = np.matmul((1-self.y).T,np.log(1.0-self.getHypothesis()))
         m = len(self.y)
         
         # Final Calculation
         return (1/m) * np.sum(sum1-sum2)
     
+    # Setting the Alpha
+    def setAlpha(self,a):
+        self.alpha = a
+        
+    # Setting the Iterations
+    def setIterations(self,i):
+        self.iterations = i
+        
     # Getting the Gradient
     def gradient(self):
         h = self.getHypothesis()
-        m = self.X.shape[0]
+        m = len(self.y)
         error = h - self.y
         
         return (1/m) * np.matmul(error.T,self.X).T
+    
+    # Deploying Gradient Descent
+    def gradientDescent(self):
+         
+        for i in range(1,self.iterations):
+            grad = self.gradient()
+            self.theta = np.subtract(self.theta,self.alpha*grad)
+            self.cost.append(self.getCost())
+            self.iterat.append(i)
+            
+            # Makes sure the cost is not increasing
+            try:
+                if self.cost[i] < self.cost[i+1]:
+                    return('Gradient Descent is not working properly!')
+            except:
+                pass
+            
+           
+    # Predict Method
+    def predict(self,*args):
+        # Calling gradient descent if needed
+        if np.array_equal(self.theta,np.zeros([self.X.shape[1],1])):
+            self.gradientDescent()
+        # Column of ones
+        val = np.ones((len(args[0]),1))
+    
+        # Combining the features
+        for i in range(0,len(args)):
+            temp = args[i]
+            val = np.concatenate((val,temp),axis=1)
+       
+        # Getting the predictions(s)
+        return np.matmul(val,self.theta)
+        
+    
+    # A plot of the Cost Function 
+    def plot(self):
+        # Calling gradient descent if needed
+      if np.array_equal(self.theta,np.zeros([self.X.shape[1],1])):
+            self.gradientDescent()
+      # Plotting the graph
+      plt.plot(self.iterat,self.cost,c='blue')
+      plt.xlabel('Number of Iterations')
+      plt.ylabel('Cost Function')
+      plt.title('Cost Function v.s the Number of Iterations')
