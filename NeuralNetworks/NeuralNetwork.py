b80622789b45ea1005b5ec80d17da451eeafce57
Jinal.Shah <jinujets1106@gmail.com>
2020-05-16 00:46:12 -0400

    Creating the Neural Network class

---
+++ NeuralNetworks/NeuralNetwork.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author Jinal Shah
+
+
+This file will house the Neural Network Class.
+I will be building a 3 layer (1 input, 1 hidden, and 1 output)
+Neural Network. I am implementing this for understanding and 
+for fun!
+
+"""
+# Importing Libraries
+import numpy as np
+
+class NeuralNetwork():
+    
+    # Constructor
+    def __init__(self):
+        pass
