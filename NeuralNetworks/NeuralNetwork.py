e75208ed51b8e62e58b8a24177c23f75a0da91e6
Jinal.Shah <jinujets1106@gmail.com>
2020-05-18 00:42:38 -0400

    Starting Neural Network Code

--- NeuralNetworks/NeuralNetwork.py
+++ NeuralNetworks/NeuralNetwork.py
@@ -8,7 +8,9 @@
 This file will house the Neural Network Class.
 I will be building a 3 layer (1 input, 1 hidden, and 1 output)
 Neural Network. I am implementing this for understanding and 
-for fun!
+for fun! 
+
+Final note, all activation functions will be sigmoid
 
 """
 # Importing Libraries
@@ -18,4 +20,21 @@ class NeuralNetwork():
     
     # Constructor
     def __init__(self):
-        pass
+        # Declaring my biases
+        self.b1 = 1
+        self.b2 = 1
+        
+    
+    # Fit Method
+    def fit(self,X,y):
+        self.X = X
+        self.y = y
+        
+        # Declaring my weights
+        self.weights1 = np.random.random(3,1)
+        self.weights2 = np.random.random(3,1)
+        self.weights3 = np.random.random(3,1)
+    
+    # Sigmoid Function Method
+    def sigmoid(self,z):
+        return 1/(1 + np.exp(-z))
